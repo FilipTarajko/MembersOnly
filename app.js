e74e2e91588dc2bbbec9ab8b343c2f3d87fdd10d
FilipTarajko <filiptarajko@gmail.com>
2021-06-19 17:09:15 +0200

    Prepare for deployment and fix errors

--- app.js
+++ app.js
@@ -74,9 +74,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', router);
+app.use(compression());
 
 app.use(function(req, res, next) {
   next(createError(404));
